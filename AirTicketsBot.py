6b429cc615fb700bfc4e64451c1cb56adaf53055
gukiboy <gustavokishima@gmail.com>
2016-01-07 11:44:14 +0000

    Keeps bot running

--- AirTicketsBot.py
+++ AirTicketsBot.py
@@ -1,9 +1,16 @@
 import telepot
+import time
 from pprint import pprint
 
-bot = telepot.Bot('176703220:AAGLqtZ531Lbmjguzv-9W4ui9CL3sKXgYK8');
+bot = telepot.Bot('176703220:AAGLqtZ531Lbmjguzv-9W4ui9CL3sKXgYK8')
+
+print "Running AirTickets"
+
 
 def handle_message(msg):
 	pprint(msg)
 
 bot.notifyOnMessage(handle_message)
+
+while 1:
+	time.sleep(10)
