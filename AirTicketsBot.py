2773a7044ad320872556b5b51fd8432ae01a6b83
gukiboy <gustavokishima@gmail.com>
2016-01-11 18:08:47 +0000

    Object programmed AirTicketsBot

--- AirTicketsBot.py
+++ AirTicketsBot.py
@@ -4,14 +4,13 @@ from pprint import pprint
 
 class MyBot(telepot.Bot):
 	def handle(self,msg):
-	        receivedText  = msg[u'text']
+	        receivedText  = msg[u'text'] 
 	        messageSender = msg[u'from'][u'id']
 
         	self.sendMessage(messageSender, receivedText)
 
-def main():
-	bot = MyBot('176703220:AAGLqtZ531Lbmjguzv-9W4ui9CL3sKXgYK8')
-	bot.notifyOnMessage()
+	def listenNotification(self):
+		self.notifyOnMessage()
 
-	while 1:
-		time.sleep(10)
+		while 1:
+			time.sleep(10)
